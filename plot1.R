54c9a4f028af93cb83cc844f1a02d6ed3b2a8978
Masahiro Hiramatsu <masahiro.hiramatsu.ccna@gmail.com>
2015-01-10 22:27:17 -0200

    Revert "plot 1 - deliverables"
    This reverts commit df161bc9b143acc8770c1d36cf6b89726a4c5100.

--- plot1.R
+++
@@ -1,6 +0,0 @@
-## draw/plot the graph
-hist(power3$Global_active_power, main = paste("Global Active Power"), col="red", xlab="Global Active Power (kilowatts)")
-## generate the archive, need to finish process to graph archive to be created
-dev.copy(png, file="plot1.png", width=480, height=480)
-## dev.off() creates the .png image file
-dev.off()
